89e2b85843ed99acb69d4b1793a9f238fa90baf9
t4ce <jonasb@post.com>
2015-04-27 21:32:52 +0200

    Initial load

--- app/src/main/java/org/jonas/splitfit/MainActivity.java
+++ app/src/main/java/org/jonas/splitfit/MainActivity.java
@@ -1,23 +1,17 @@
 package org.jonas.splitfit;
 
 import android.app.Activity;
-
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-//blabla
+
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
