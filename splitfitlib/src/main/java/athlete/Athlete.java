4dc9b2692aaeccf17989d5dbd1d263e9a0965e05
t4ce <jonasb@post.com>
2015-05-06 13:31:49 +0200

    splitfitlib is now the general java lib for our business logic

--- splitfitlib/src/main/java/athlete/Athlete.java
+++ splitfitlib/src/main/java/athlete/Athlete.java
@@ -1,9 +1,9 @@
-package user;
+package athlete;
 
 /**
  * Created by jonas on 06/05/2015.
  * Not to be read or used by anyone but jonas.
  */
-public interface User {
+public interface Athlete {
 
 }
